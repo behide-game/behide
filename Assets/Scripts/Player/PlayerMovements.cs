7aa90b0f626d9eba60d6317bb8aadc64c4bfc73c
Gaël Bouquain <gael.bouquain@gmail.com>
2023-01-15 17:31:09 +0100

    Fix player movements

--- Assets/Scripts/Player/PlayerMovements.cs
+++ Assets/Scripts/Player/PlayerMovements.cs
@@ -15,26 +15,26 @@ public class PlayerMovements : NetworkBehaviour
     public GameObject playerCam;
     [HideInInspector]
     public Rigidbody rb;
-    private float rotateY = 0f;
+    private float currentVerticalRotation = 0f;
 
-    private void FixedUpdate()
+    private void Update()
     {
         if (playerCam == null || rb == null || !isLocalPlayer) return;
 
         // Movement
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveX, 0, moveZ).normalized * (Time.fixedDeltaTime * moveSpeed);
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
+        Vector3 movement = new Vector3(moveX, 0, moveZ).normalized * Time.deltaTime * moveSpeed;
         transform.Translate(movement);
 
         // Rotation
-        float rotateX = Input.GetAxisRaw("Mouse X") * Time.fixedDeltaTime * mouseXSensitivity;
-        rotateY -= Input.GetAxisRaw("Mouse Y") * Time.fixedDeltaTime * mouseYSensitivity;
-        rotateY = Mathf.Clamp(rotateY, -90, 90);
+        float rotateX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * mouseXSensitivity;
+        currentVerticalRotation -= Input.GetAxisRaw("Mouse Y") * Time.deltaTime * mouseYSensitivity;
+        currentVerticalRotation = Mathf.Clamp(currentVerticalRotation, -90, 90);
 
         // Rotate (left/right) the body
         transform.Rotate(new Vector3(0, rotateX, 0));
         // Rotate (top/bottom) the camera
-        playerCam.transform.localEulerAngles = new Vector3(rotateY, 0, 0);
+        playerCam.transform.localEulerAngles = new Vector3(currentVerticalRotation, 0, 0);
     }
 }
