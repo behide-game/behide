db7587752fd96036c6257165eddfbfff290721d3
Gaël Bouquain <gael.bouquain@gmail.com>
2023-01-25 16:55:42 +0100

    Add the ability to players to generate a spatialized sound
    - Add the ability to players to generate a spatialized sound
    - Add a prop

---
+++ Assets/Scripts/Player/PlayerAudioManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using Mirror;
+
+public class PlayerAudioManager : NetworkBehaviour
+{
+    [SerializeField] private AudioClip[] audios;
+    [SerializeField] private AudioSource audioSource;
+    private bool playing = false;
+
+    private void Update()
+    {
+        if (playing && !audioSource.isPlaying) playing = false;
+    }
+
+    public void Play(int audioIndex) { if (!playing) PlayCmd(audioIndex); }
+
+    [Command]
+    private void PlayCmd(int audioIndex) => PlayRpc(audioIndex);
+
+    [ClientRpc]
+    private void PlayRpc(int audioIndex)
+    {
+        Debug.Log("From: " + this.name);
+
+        AudioClip audio = audios[audioIndex];
+        audioSource.PlayOneShot(audio);
+        playing = true;
+    }
+}
