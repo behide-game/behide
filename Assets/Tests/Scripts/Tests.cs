8fa5228bffef7842d6a94d419c4d4e951b75099d
Gaël Bouquain <gael.bouquain@gmail.com>
2023-07-26 22:18:10 +0200

    Update test reporter in github action

--- Assets/Tests/Scripts/Tests.cs
+++ Assets/Tests/Scripts/Tests.cs
@@ -20,12 +20,14 @@ public class Tests
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
 
-        yield return Common.waitUntilOrTimeout(20_000, () =>
+        yield return Common.waitUntilOrTimeout(25_000, () =>
             gameManager.network.behideConnected()
             && gameManager.network.eosConnected()
         );
 
         Assert.IsTrue(gameManager.network.behideConnected(), "Should be connected to behide's server");
         Assert.IsTrue(gameManager.network.eosConnected(), "Should be connected to EOS");
+        Assert.IsTrue(gameManager.network.epicId.Success, "There should be an epicId");
+        Assert.IsTrue(gameManager.network.epicId.Value != null, "There should be an epicId");
     }
 }
