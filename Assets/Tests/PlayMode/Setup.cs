4ad91796c1aa36732c5e6365d62dc1c1a7834d02
Gaël Bouquain <gael.bouquain@gmail.com>
2023-08-06 12:10:26 +0200

    Test

--- Assets/Tests/PlayMode/Setup.cs
+++ Assets/Tests/PlayMode/Setup.cs
@@ -7,38 +7,38 @@ using UnityEngine.TestTools;
 using UnityEditor.SceneManagement;
 #endif
 
-public class SetupClass : IPrebuildSetup
-{
-    string[] scenesToEdit = new string[]
-    {
-        "Assets/Scenes/Start screen/Start screen.unity",
-        "Assets/Tests/PlayMode/Scene/Tests scene.unity"
-    };
+// public class SetupClass : IPrebuildSetup
+// {
+//     string[] scenesToEdit = new string[]
+//     {
+//         "Assets/Scenes/Start screen/Start screen.unity",
+//         "Assets/Tests/PlayMode/Scene/Tests scene.unity"
+//     };
 
-    public void Setup()
-    {
-#if UNITY_EDITOR
-        foreach (var scenePath in scenesToEdit)
-        {
-            var scene = EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Additive);
+//     public void Setup()
+//     {
+// #if UNITY_EDITOR
+//         foreach (var scenePath in scenesToEdit)
+//         {
+//             var scene = EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Additive);
 
-            try
-            {
-                GameObject.Find("InputManager")
-                    .GetComponent<InputSystemUIInputModule>()
-                    .enabled = false;
-            }
-            catch (Exception) { }
+//             try
+//             {
+//                 GameObject.Find("InputManager")
+//                     .GetComponent<InputSystemUIInputModule>()
+//                     .enabled = false;
+//             }
+//             catch (Exception) { }
 
-            EditorSceneManager.SaveScene(scene);
-            EditorSceneManager.CloseScene(scene, true);
-        }
-#endif
-    }
+//             EditorSceneManager.SaveScene(scene);
+//             EditorSceneManager.CloseScene(scene, true);
+//         }
+// #endif
+//     }
 
-    [UnityTest]
-    public IEnumerator Test()
-    {
-        yield return null;
-    }
-}
+//     [UnityTest]
+//     public IEnumerator Test()
+//     {
+//         yield return null;
+//     }
+// }
