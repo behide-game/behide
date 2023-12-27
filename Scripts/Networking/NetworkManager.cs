05c123e79b03adb34b01006f7fab54772a83dc96
Gaël Bouquain <gael.bouquain@gmail.com>
2023-12-27 15:49:25 +0100

    Switch to peer to peer networking
    All clients connect to all other clients

--- Scripts/Networking/NetworkManager.cs
+++ Scripts/Networking/NetworkManager.cs
@@ -3,33 +3,30 @@ namespace Behide.Networking;
 using Godot;
 using Behide.OnlineServices;
 using System.Threading.Tasks;
+using System.Linq;
 
 public partial class NetworkManager : Node3D
 {
     private Signaling signaling = null!;
     private WebRtcMultiplayerPeer multiplayer = new();
-    private int nextPeerId = 2;
 
     public override void _EnterTree()
     {
         signaling = GetNode<Signaling>("/root/WebRtcSignaling");
-        multiplayer.PeerConnected += peerId => GD.Print("Peer connected: ", peerId);
     }
 
     public async Task<Result<RoomId>> StartHost()
     {
-        multiplayer.CreateServer();
+        multiplayer.CreateMesh(1);
         Multiplayer.MultiplayerPeer = multiplayer;
 
         // Handle new connection
-        signaling.OfferIdCreationRequested += async () =>
+        signaling.OfferIdCreationRequested += async askingPeerId =>
         {
             var peer = new OfferPeerConnection(signaling);
-            var peerId = nextPeerId++;
+            multiplayer.AddPeer(peer.GetPeerConnection(), askingPeerId);
 
-            multiplayer.AddPeer(peer.GetPeerConnection(), peerId);
-
-            return (peerId, await peer.CreateOffer());
+            return await peer.CreateOffer();
         };
 
         return await signaling.CreateRoom();
@@ -38,20 +35,35 @@ public partial class NetworkManager : Node3D
     public async Task StartClient(RoomId roomId)
     {
         // Retrieve offer and peer id
-        var (peerId, offerId) = await signaling.JoinRoom(roomId) switch
+        var joinRoomInfo = await signaling.JoinRoom(roomId) switch
         {
-            Result<(int, OfferId)>.Ok res => res.Value,
-            Result<(int, OfferId)>.Error error => throw new System.Exception("Failed to retrieve offerId: " + error.Failure),
+            Result<JoinRoomRes>.Ok res => res.Value,
+            Result<JoinRoomRes>.Error error => throw new System.Exception("Failed to retrieve offer ids: " + error.Failure),
             _ => throw new System.NotImplementedException()
         };
 
-        multiplayer.CreateClient(peerId);
+        multiplayer.CreateMesh(joinRoomInfo.PeerId);
         Multiplayer.MultiplayerPeer = multiplayer;
 
-        // Connect to host
-        var peer = new AnswerPeerConnection(signaling, offerId);
-        multiplayer.AddPeer(peer.GetPeerConnection(), 1);
+        // Handle following players connection
+        signaling.OfferIdCreationRequested += async askingPeerId =>
+        {
+            var peer = new OfferPeerConnection(signaling);
+            multiplayer.AddPeer(peer.GetPeerConnection(), askingPeerId);
 
-        await peer.Connect();
+            return await peer.CreateOffer();
+        };
+
+        // Connect to other players
+        var tasks = joinRoomInfo.PlayersConnectionInfo.Select(async connInfo =>
+        {
+            GameManager.Ui.Log($"Connecting to {connInfo.PeerId}");
+            var peer = new AnswerPeerConnection(signaling, connInfo.OfferId);
+            multiplayer.AddPeer(peer.GetPeerConnection(), connInfo.PeerId);
+
+            await peer.Connect();
+        });
+
+        await Task.WhenAll(tasks);
     }
 }
