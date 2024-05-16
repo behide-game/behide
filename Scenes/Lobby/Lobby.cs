5c5dfa71d6a8bfd93e2dc470024ed48809970b21
Gaël Bouquain <gael.bouquain@gmail.com>
2024-05-16 23:09:21 +0200

    Users in room can notify they are ready. Added a countdown to start game

--- Scenes/Lobby/Lobby.cs
+++ Scenes/Lobby/Lobby.cs
@@ -4,8 +4,52 @@ using Godot;
 using Behide.Types;
 using Behide.OnlineServices.Signaling;
 using Serilog;
+using System;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
+
+public class Countdown(int durationInSec)
+{
+    private readonly int initialDuration = durationInSec;
+    private int timeLeft = 0;
+    private bool isRunning = false;
+    private CancellationTokenSource? cts;
+
+    public event Action<int>? Tick;
+    public event Action? Canceled;
+    public event Action? Finished;
+
+    public async void Start()
+    {
+        if (isRunning) return;
+
+        timeLeft = initialDuration;
+        isRunning = true;
+        cts = new CancellationTokenSource();
+        cts.Token.Register(() => Canceled?.Invoke());
+
+        while (timeLeft > 0 && !cts.Token.IsCancellationRequested)
+        {
+            Tick?.Invoke(timeLeft);
+            await Task.Delay(1000, cts.Token);
+            timeLeft--;
+        }
+
+        if (!cts.Token.IsCancellationRequested)
+            Finished?.Invoke();
+    }
+
+    public void Stop()
+    {
+        if (!isRunning) return;
+
+        cts?.Cancel();
+        isRunning = false;
+        timeLeft = initialDuration;
+    }
+}
 
 public partial class Lobby : Control
 {
@@ -13,6 +57,9 @@ public partial class Lobby : Control
     private Control lobbyControl = null!;
 
     private ILogger Log = null!;
+
+    private readonly Countdown countdown = new(5);
+
 
     public override void _EnterTree()
     {
@@ -23,6 +70,13 @@ public partial class Lobby : Control
 
         chooseModeControl.Visible = true;
         lobbyControl.Visible = false;
+
+        var countdownLabel = GetNode<Label>("Lobby/Header/Countdown");
+        var countdownDefaultText = countdownLabel.Text;
+
+        countdown.Tick += timeLeft => countdownLabel.Text = $"Starting in {timeLeft}s";
+        countdown.Canceled += () => countdownLabel.Text = countdownDefaultText;
+        countdown.Finished += () => countdownLabel.Text = "Finished";
     }
 
     private void ShowLobby(RoomId roomId)
@@ -34,6 +88,7 @@ public partial class Lobby : Control
         GameManager.Room.players.ForEach(OnPlayerRegistered);
         GameManager.Room.PlayerRegistered += OnPlayerRegistered;
         GameManager.Room.PlayerLeft += OnPlayerLeft;
+        GameManager.Room.PlayerReady += OnPlayerReady;
     }
 
     private void HideLobby()
@@ -43,12 +98,12 @@ public partial class Lobby : Control
 
         GameManager.Room.PlayerRegistered -= OnPlayerRegistered;
         GameManager.Room.PlayerLeft -= OnPlayerLeft;
+        GameManager.Room.PlayerReady -= OnPlayerReady;
+
+        countdown.Stop();
 
         var playerControls = GetNode<VBoxContainer>("Lobby/Boxes/Players/VerticalAligner/ScrollContainer/Players").GetChildren();
-        foreach (var playerControl in playerControls)
-        {
-            playerControl.QueueFree();
-        }
+        foreach (var playerControl in playerControls) playerControl.QueueFree();
     }
 
 
@@ -91,7 +146,8 @@ public partial class Lobby : Control
     private void OnPlayerRegistered(Behide.Player player)
     {
         var playerList = GetNode<VBoxContainer>("Lobby/Boxes/Players/VerticalAligner/ScrollContainer/Players"); // TODO: Put this kind of thing in a variable editable in the Godot editor
-        var playerLabel = new Label { Text = player.Username };
+        var playerLabel = new Label { Name = player.Username.Replace(":", "_"), Text = $"{player.Username}: Not ready" }; // TODO: Find a solution for Replace(":", "_")
+        GD.Print(player.Username);
         playerList.AddChild(playerLabel);
     }
 
@@ -102,10 +158,39 @@ public partial class Lobby : Control
         playerList.RemoveChild(playerLabel);
     }
 
-
-    private void OnStartButtonPressed()
+    private void OnPlayerReady(Behide.Player player, bool ready)
     {
-        Log.Error("Not implemented yet");
+        var playerList = GetNode<VBoxContainer>("Lobby/Boxes/Players/VerticalAligner/ScrollContainer/Players").GetChildren();
+        var playerLabel = (Label?)playerList.FirstOrDefault(child => {
+            GD.Print(child.Name);
+            return child.Name == player.Username.Replace(":", "_");
+        });
+
+        if (playerLabel is null)
+        {
+            Log.Error("Player {Username} not found", player.Username);
+            return;
+        }
+
+        playerLabel.Text = $"{player.Username}: {(ready ? "Ready" : "Not ready")}";
+
+        if (ready == true)
+        {
+            var allReady = GameManager.Room.players.All(player => player.Ready);
+            if (allReady)
+                countdown.Start();
+        }
+        else
+            countdown.Stop();
+    }
+
+
+    private void OnReadyButtonPressed()
+    {
+        GameManager.Room.ToggleReady();
+
+        var readyControl = GetNode<Button>("Lobby/Boxes/VBoxContainer/HBoxContainer/Ready");
+        readyControl.Text = GameManager.Room.localPlayer?.Ready ?? false ? "Ready" : "Not ready";
     }
 
     private void OnQuitButtonPressed()
