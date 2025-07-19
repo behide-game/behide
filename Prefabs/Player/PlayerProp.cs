f6ecba7f8235fbdc274d2a7531b0c93345593eef
TheRedTanuki <petitmars@outlook.com>
2025-07-20 01:07:54 +0200

    simplifying morph

--- Prefabs/Player/PlayerProp.cs
+++ Prefabs/Player/PlayerProp.cs
@@ -5,7 +5,7 @@ namespace Behide.Game.Player;
 public partial class PlayerProp : PlayerMovements
 {
     [Export] private Node3D currentVisualNode = null!;
-    [Export] public CollisionShape3D[] CollisionNodes = null!;
+    [Export] private CollisionShape3D[] CollisionNodes = null!;
     [Export] private RayCast3D RayCast = null!;
     private BehideObject? focusedBehideObject;
 
@@ -25,17 +25,13 @@ public partial class PlayerProp : PlayerMovements
         if (Input.IsActionJustPressed("morph") && focusedBehideObject is not null)
         {
             Node candidate = focusedBehideObject.VisualNode.Duplicate();
-            if (candidate is Node3D)
+            if (candidate is Node3D newVisualNode)
             {
-                Node3D? newMesh = candidate as Node3D;
-                if (newMesh is not null)
-                {
-                    currentVisualNode.QueueFree();
-                    RemoveChild(currentVisualNode);
-                    AddChild(newMesh);
-                    newMesh.Position = Vector3.Zero;
-                    currentVisualNode = newMesh;
-                }
+                currentVisualNode.QueueFree();
+                RemoveChild(currentVisualNode);
+                AddChild(newVisualNode);
+                newVisualNode.Position = Vector3.Zero;
+                currentVisualNode = newVisualNode;
             }
         }
     }
