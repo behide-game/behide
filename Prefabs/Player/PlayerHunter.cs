88890c61608ca339f144d6fb7bd9c00128321a79
TheRedTanuki <petitmars@outlook.com>
2025-07-27 01:16:58 +0200

    Implement raycast for hunter's gun

--- Prefabs/Player/PlayerHunter.cs
+++ Prefabs/Player/PlayerHunter.cs
@@ -7,6 +7,8 @@ public partial class PlayerHunter : PlayerBody
 {
     private RayCast3D rayCastView = null!;
     private RayCast3D rayCastGun = null!;
+    private const float rayLength = 1000.0f;
+    private Vector2 winSize;
 
     protected override void InitializeNodes()
     {
@@ -20,6 +22,26 @@ public partial class PlayerHunter : PlayerBody
     {
         base._EnterTree();
         Health = 100;
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+        if (Input.IsActionJustPressed("morph"))
+        {
+            winSize = GetViewport().GetVisibleRect().Size;
+            var spaceState = GetWorld3D().DirectSpaceState;
+
+            var from = Camera.ProjectRayOrigin(winSize / 2);
+            var to = from + Camera.ProjectRayNormal(winSize / 2) * rayLength;
+
+            var query = PhysicsRayQueryParameters3D.Create(from, to);
+            var result = spaceState.IntersectRay(query);
+            //if (result["collider"] is PlayerProp)
+            //{
+            var collider = result["collider"].As<Node>();
+            if (collider is PlayerProp) GD.Print("Hit");
+        }
     }
 
     public override void _Input(InputEvent rawEvent)
