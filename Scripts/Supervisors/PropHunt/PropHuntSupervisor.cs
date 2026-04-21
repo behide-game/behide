4d18e7d552e025ca2dd4799b0e85ade31e9c9754
Gaël Bouquain <gael.bouquain@gmail.com>
2026-04-21 10:48:54 +0200

    Change timespans when debugging

--- Scripts/Supervisors/PropHunt/PropHuntSupervisor.cs
+++ Scripts/Supervisors/PropHunt/PropHuntSupervisor.cs
@@ -43,8 +43,13 @@ public partial class PropHuntSupervisor : Supervisor
     private Spectator Spectator => nodes.Spectator;
 
     private readonly Serilog.ILogger log = Log.CreateLogger("Supervisor/PropHunt");
+#if DEBUG
+    private static readonly TimeSpan preGameDuration = TimeSpan.FromSeconds(2);
+    private static readonly TimeSpan inGameDuration = TimeSpan.FromMinutes(5);
+#else
     private static readonly TimeSpan preGameDuration = TimeSpan.FromSeconds(30);
     private static readonly TimeSpan inGameDuration = TimeSpan.FromMinutes(7);
+#endif
 
     public EventHandler<int[]>? HuntersChose;
     private int[]? hunterPeerIds;
