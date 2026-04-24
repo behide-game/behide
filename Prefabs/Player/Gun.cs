0c56978fa9e131741be46dea201e874eb16eb5f2
Hmnd <Xavier.bouquain@gmail.com>
2026-04-24 17:35:35 +0200

    Changed gun and hunter working
    - Add ammo system (including reload)
    - Add sounds when shooting/reloading

---
+++ Prefabs/Player/Gun.cs
@@ -0,0 +1,111 @@
+using Behide.Game;
+using Behide.Game.Player;
+using Behide.Game.Supervisors;
+using Godot;
+
+namespace Behide.Prefabs.Player;
+
+public abstract partial class Gun : Node3D
+{
+    protected HunterBody Hunter = null!;
+    public Control Hud = null!;
+    protected RayCast3D Raycast = null!;
+    protected Label PlayerUsername = null!;
+    protected Control CrosshairHit = null!;
+    protected Label AmmoLabel = null!;
+
+    protected abstract int magazineSize { get; }
+    protected abstract float reloadTime { get; }
+    protected abstract float fireRate { get; }
+
+    protected int ammoCount
+    {
+        get;
+        set
+        {
+            field = value;
+            AmmoLabel.Text = value + " / " + totalAmmoCount;
+        }
+    }
+    protected int totalAmmoCount;
+    protected internal int damagePerAmmo;
+    protected double crosshairHitDuration;
+
+    private bool canShoot => timerFire <= 0 && ammoCount > 0;
+    private bool canReload => timerReload <= 0 && ammoCount < magazineSize && totalAmmoCount > 0;
+
+    private GodotObject? focusedObject;
+    private Tween? crosshairHitTween;
+    private double timerFire;
+    private double timerReload;
+
+    public abstract void InitializeNodes(HunterBody hunter);
+    public abstract void InitializeProperties();
+
+    public override void _Process(double delta)
+    {
+        focusedObject = Raycast.GetCollider();
+
+        // Set player name in HUD
+        if (focusedObject is HunterBody hunterBody)
+        {
+            var owner = Hunter.Supervisor.GetBodyPlayer(hunterBody);
+            PlayerUsername.Text = owner?.Username;
+        }
+        else
+            PlayerUsername.Text = string.Empty;
+
+        if (timerFire > 0) timerFire -= delta;
+
+        if (timerReload <= 0) return;
+        if (timerReload - delta <= 0)
+        {
+            var ammoDiff = magazineSize - ammoCount;
+            var realAmmoAmountAdded = int.Min(ammoDiff, totalAmmoCount);
+            totalAmmoCount -= realAmmoAmountAdded;
+            ammoCount += realAmmoAmountAdded;
+        }
+        timerReload -= delta;
+    }
+
+    public Node3D? TryShoot()
+    {
+        if (canShoot) return Shoot();
+        if (timerFire <= 0 && timerReload <= 0 && ammoCount == 0)
+            TryReload();
+        return null;
+    }
+
+    protected virtual Node3D? Shoot()
+    {
+        timerFire = 1 / fireRate;
+        ammoCount -= 1;
+        switch (focusedObject)
+        {
+            case PropBody player:
+                crosshairHitTween?.Kill();
+                crosshairHitTween = CreateTween();
+                crosshairHitTween.SetEase(Tween.EaseType.In);
+                crosshairHitTween.TweenProperty(CrosshairHit, "modulate", new Color(0xFFFFFF00), crosshairHitDuration);
+                CrosshairHit.Modulate = new Color(0xFFFFFFFF);
+                return player;
+            case BehideObject behideObject:
+                return behideObject;
+            default:
+                return null;
+        }
+    }
+
+    public void TryReload()
+    {
+        if (!canReload) return;
+        Reload();
+    }
+
+    protected virtual void Reload()
+    {
+        timerReload = reloadTime;
+        AmmoLabel.Text = "Reloading";
+        // + Animations
+    }
+}
---
+++ Prefabs/Player/SubmachineGun.cs
@@ -0,0 +1,49 @@
+using Behide.Game;
+using Behide.Game.Player;
+using Behide.Game.Supervisors;
+using Godot;
+using Serilog;
+
+namespace Behide.Prefabs.Player;
+
+[SceneTree("SubmachineGun.tscn")]
+public partial class SubmachineGun : Gun
+{
+    [Export] private AudioStream shootSound = null!;
+    [Export] private AudioStream reloadSound = null!;
+    protected override int magazineSize => 30;
+    protected override float fireRate => 10f;
+    protected override float reloadTime => 2.5f;
+
+    public override void InitializeNodes(HunterBody hunter)
+    {
+        Hunter = hunter;
+        Hud = _.Hud;
+        Raycast = _.RayCast;
+        PlayerUsername = _.Hud.Center.PlayerUsername;
+        CrosshairHit = _.Hud.Center.Crosshair.CrosshairHit;
+        AmmoLabel = _.Hud.RBottom.Ammo;
+    }
+
+    public override void InitializeProperties()
+    {
+        damagePerAmmo = 1;
+        totalAmmoCount = 40;
+        ammoCount = 30;
+        crosshairHitDuration = 0.3d;
+    }
+
+    protected override Node3D? Shoot()
+    {
+        _.AudioStreamPlayer3D.Stream = shootSound;
+        _.AudioStreamPlayer3D.Play();
+        return base.Shoot();
+    }
+
+    protected override void Reload()
+    {
+        base.Reload();
+        _.AudioStreamPlayer3D.Stream = reloadSound;
+        _.AudioStreamPlayer3D.Play();
+    }
+}
